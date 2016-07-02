b2ca7270163ee5686c3d4ff1bea7be778bfd7cc0
Prateek Bagrecha <bagrecha.prateek@gmail.com>
2016-07-02 13:26:13 +0200

    Suggestion Scheme and Routes Added

--- suggest/suggestRoutes.js
+++ suggest/suggestRoutes.js
@@ -2,16 +2,10 @@ module.exports = suggestRoutes;
 
 function suggestRoutes(passport) {
 
-    var suggestController = require('./patternController');
+    var suggestController = require('./suggestController');
     var router = require('express').Router();
     var unless = require('express-unless');
 
-    /*
-    * Routes Required
-    * 1) Get All Patterns
-    * 2) Get Pattern By Id
-    * 3) Get Pattern By Name
-    * */
 
     var mw = passport.authenticate('jwt', {session: false});
     mw.unless = unless;
